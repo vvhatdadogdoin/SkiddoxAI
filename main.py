89e4ad27d104965064f8ae49ca016bf2495d96f1
vvhatdadogdoin <jifdosggfdoijg@gmail.com>
2025-01-11 15:49:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -226,5 +226,5 @@ def main2():
 	app.run(host = "0.0.0.0", port = 5000, debug = False, use_reloader = False)
 
 if __name__ == "__main__": 
-	app.run(host = "0.0.0.0", port = 5000, debug = False, use_reloader = False)
 	bot.run(token)
+	app.run(host = "0.0.0.0", port = 5000, debug = False, use_reloader = False)
